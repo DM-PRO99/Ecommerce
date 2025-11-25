dac038c6c67af8211fcbde3dc2042b2d10906aea
Diego Mena <diegomena903@gmail.com>
2025-11-25 18:11:16 -0500

    Cypress login

---
+++ cypress/support/index.d.ts
@@ -0,0 +1,9 @@
+/// <reference types="cypress" />
+
+declare global {
+  namespace Cypress {
+    interface Chainable<Subject> {
+      login(email?: string, password?: string): Chainable<Subject>;
+    }
+  }
+}
